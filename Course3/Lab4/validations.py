a5ce22f532ead10008200eeb3a5902862e9c5b79
mpahnaf <mpahnaf@gmail.com>
2020-07-17 05:37:02 +0000

    modified

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -4,8 +4,8 @@ import re
 
 def validate_user(username, minlen):
     """Checks if the received username matches the required conditions."""
-    if type(username) != str:
-        raise TypeError("username must be a string")
+    if type(username) != str and !username.startswith('{'):
+        raise TypeError("username must be a string and starts with a valid character")
     if minlen < 1:
         raise ValueError("minlen must be at least 1")
     
@@ -22,3 +22,7 @@ def validate_user(username, minlen):
 
 
 
+print(validate_user("blue.kale", 3)) # True
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
